6eda312ef746b7d62e4b80f0174ce44e031a48b8
joacomesa22 <joacomesa@gmail.com>
2024-12-03 13:02:40 -0500

    db done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Joi = require("joi");
 app.use(cors());
 app.use(express.static("public"));
 const multer = require("multer");
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -17,75 +18,95 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const news = [
-  {
-    _id: 1,
-    title: "Lionel Messi's luxurious house in Barcelona",
-    img: "mansion.jpg",
-    link: "https://www.youtube.com/watch?app=desktop&v=ncxD-WXorrA",
-    category: ["Lifestyle", "Barcelona"],
-  },
-  {
-    _id: 2,
-    title: "The day Messi scored with just one boot ",
-    img: "farsa.jpg",
-    link: "https://www.givemesport.com/88097980-lionel-messis-epic-barcelona-assist-with-just-one-boot-vs-real-madrid-and-ronaldo/",
-    category: ["Stories", "Football", "Barcelona"],
-  },
-  {
-    _id: 3,
-    title: "Did you know Messi was sent off in his debut?",
-    img: "roja.jpg",
-    link: "https://www.tycsports.com/seleccion-argentina/debut-de-lionel-messi-en-la-seleccion-argentina-ante-hungria-id529348.html",
-    category: ["Fun Facts", "Argentina", "Football"],
-  },
-  {
-    _id: 4,
-    title: "Lionel's tribute to Diego Armando Maradona ",
-    img: "diego.jpeg",
-    link: "https://www.theguardian.com/football/blog/2020/nov/30/lionel-messi-newells-old-boys-shirt-maradona-barcelona",
-    category: ["Stories", "Football", "Barcelona", "Argentina"],
-  },
-  {
-    _id: 5,
-    title: "Argentina's Lionel Messi blames soaked pitch",
-    img: "soaked.jpg",
-    link: "https://www.espn.com/soccer/story/_/id/41724998/argentina-lionel-messi-soaked-pitch-venezuela-draw",
-    category: ["Argentina", "Football", "Stories"],
-  },
-  {
-    _id: 6,
-    title: "The 10 players who can wear Messi's new boots",
-    img: "boots.jpg",
-    link: "https://www.sportbible.com/football/mls/lionel-messi-inter-miami-adidas-lamine-yamal-278908-20241011",
-    category: ["Lifestyle", "Fun Facts", "MLS"],
-  },
-  {
-    _id: 7,
-    title: "Lionel Messi is nominated for MLS' MVP award",
-    img: "inter.jpg",
-    link: "https://www.dailymail.co.uk/sport/football/article-13942757/lionel-messi-mls-mvp-award-inter-miami.html",
-    category: ["MLS", "Football", "Stories"],
-  },
-  {
-    _id: 8,
-    title: "Huge fan risking it all to meet Lio",
-    img: "invader.jpg",
-    link: "https://www.miamiherald.com/sports/mls/inter-miami/article293671764.html",
-    category: ["Stories", "Fun Facts", "MLS"],
-  },
-];
+mongoose
+  .connect(
+    "mongodb+srv://joaquin22:ntmrzCiHGMayO70s@messinews.udlti.mongodb.net/?retryWrites=true&w=majority&appName=MessiNews"
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("couldn't connect to mongodb", error);
+  });
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  img: String,
+  link: String,
+  category: [String],
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+// const news = [
+//   {
+//     _id: 1,
+//     title: "Lionel Messi's luxurious house in Barcelona",
+//     img: "mansion.jpg",
+//     link: "https://www.youtube.com/watch?app=desktop&v=ncxD-WXorrA",
+//     category: ["Lifestyle", "Barcelona"],
+//   },
+//   {
+//     _id: 2,
+//     title: "The day Messi scored with just one boot ",
+//     img: "farsa.jpg",
+//     link: "https://www.givemesport.com/88097980-lionel-messis-epic-barcelona-assist-with-just-one-boot-vs-real-madrid-and-ronaldo/",
+//     category: ["Stories", "Football", "Barcelona"],
+//   },
+//   {
+//     _id: 3,
+//     title: "Did you know Messi was sent off in his debut?",
+//     img: "roja.jpg",
+//     link: "https://www.tycsports.com/seleccion-argentina/debut-de-lionel-messi-en-la-seleccion-argentina-ante-hungria-id529348.html",
+//     category: ["Fun Facts", "Argentina", "Football"],
+//   },
+//   {
+//     _id: 4,
+//     title: "Lionel's tribute to Diego Armando Maradona ",
+//     img: "diego.jpeg",
+//     link: "https://www.theguardian.com/football/blog/2020/nov/30/lionel-messi-newells-old-boys-shirt-maradona-barcelona",
+//     category: ["Stories", "Football", "Barcelona", "Argentina"],
+//   },
+//   {
+//     _id: 5,
+//     title: "Argentina's Lionel Messi blames soaked pitch",
+//     img: "soaked.jpg",
+//     link: "https://www.espn.com/soccer/story/_/id/41724998/argentina-lionel-messi-soaked-pitch-venezuela-draw",
+//     category: ["Argentina", "Football", "Stories"],
+//   },
+//   {
+//     _id: 6,
+//     title: "The 10 players who can wear Messi's new boots",
+//     img: "boots.jpg",
+//     link: "https://www.sportbible.com/football/mls/lionel-messi-inter-miami-adidas-lamine-yamal-278908-20241011",
+//     category: ["Lifestyle", "Fun Facts", "MLS"],
+//   },
+//   {
+//     _id: 7,
+//     title: "Lionel Messi is nominated for MLS' MVP award",
+//     img: "inter.jpg",
+//     link: "https://www.dailymail.co.uk/sport/football/article-13942757/lionel-messi-mls-mvp-award-inter-miami.html",
+//     category: ["MLS", "Football", "Stories"],
+//   },
+//   {
+//     _id: 8,
+//     title: "Huge fan risking it all to meet Lio",
+//     img: "invader.jpg",
+//     link: "https://www.miamiherald.com/sports/mls/inter-miami/article293671764.html",
+//     category: ["Stories", "Fun Facts", "MLS"],
+//   },
+// ];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/news", (req, res) => {
-  res.json(news);
+app.get("/api/news", async (req, res) => {
+  const news = await Article.find();
+  res.send(news);
 });
 
-app.post("/api/news", upload.single("img"), (req, res) => {
-  console.log("POST request received");
+app.post("/api/news", upload.single("img"), async (req, res) => {
   const result = validateNews(req.body);
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
@@ -93,58 +114,56 @@ app.post("/api/news", upload.single("img"), (req, res) => {
     return;
   }
 
-  const article = {
-    _id: news.length + 1,
+  const article = new Article({
     title: req.body.title,
     link: req.body.link,
     category: req.body.category,
-  };
+  });
 
   if (req.file) {
     article.img = req.file.filename;
   }
 
-  news.push(article);
-  res.status(200).send(article);
+  const newArticle = await article.save();
+
+  res.status(200).send(newArticle);
 });
 
-app.put("/api/news/:id", upload.single("img"), (req, res) => {
-  console.log("In edit");
-  console.log(req.body);
-  const article = news.find(
-    (article) => article._id === parseInt(req.params.id)
-  );
-  if (!article) {
-    return res
-      .status(404)
-      .send("The article could not be found, please try again");
-  }
+app.put("/api/news/:id", upload.single("img"), async (req, res) => {
   const result = validateNews(req.body);
 
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
     return;
   }
-  article.title = req.body.title;
-  article.link = req.body.link;
-  article.category = req.body.category;
+  const fieldsToUpdate = {
+    title: req.body.title,
+    link: req.body.link,
+    category: req.body.category,
+  };
 
   if (req.file) {
-    article.img = req.file.filename;
+    fieldsToUpdate.img = req.file.filename;
   }
+
+  const wentThrough = await Article.updateOne(
+    { _id: req.params.id },
+    fieldsToUpdate
+  );
+
+  console.log(wentThrough);
+
+  const article = await Article.findOne({ _id: req.params.id });
 
   res.status(200).send(article);
 });
 
-app.delete("/api/news/:id", (req, res) => {
-  const articleToDelete = news.find(
-    (article) => article._id === parseInt(req.params.id)
-  );
+app.delete("/api/news/:id", async (req, res) => {
+  const articleToDelete = await Article.findByIdAndDelete(req.params.id);
   if (!articleToDelete) {
     return res.status(404).send("Article not found, please try again");
   }
-  const index = news.indexOf(articleToDelete);
-  news.splice(index, 1);
+
   res.status(200).send(`Article ${articleToDelete} deleted successfully!`);
 });
 
